a13976b7569d9b28c770c290961179125dde162a
Ray Su <raysu.info@gmail.com>
2019-09-25 22:40:25 -0400

    tuple

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
-import {CsvFileReader} from './CsvFileReader'
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./matchResults";
 
-const reader =new CsvFileReader('football.csv')
+const reader = new CsvFileReader("football.csv");
 reader.read();
+
+const dateOfFirstMatch = reader.data[0][0];
+console.log("dsa", dateOfFirstMatch);
 //MU win
-// enum -  enumeration
-enum MatchResult {
-  HomeWin ='H',
-  AwayWin = 'A',
-  Draw = 'D'
+
+let manUnitedWins = 0;
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
 }
 
-let manUnitedWins =0;
-for (let match of reader.data){
-	if(match[1]=== 'Man United' && match[5] === MatchResult.HomeWin){
-	  manUnitedWins++;
-	}else if(match[2]=== 'Man United' && match[5]===MatchResult.AwayWin){
-	  manUnitedWins++
-	}
-}
-
-console.log(manUnitedWins)
+console.log(manUnitedWins);
