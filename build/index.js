02146ac17435d0d0646ff952abc355350dcc498a
Ray Su <raysu.info@gmail.com>
2019-09-25 23:14:47 -0400

    use generic

--- build/index.js
+++ build/index.js
@@ -1,20 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
-var matchResults_1 = require("./matchResults");
-var reader = new CsvFileReader_1.CsvFileReader("football.csv");
+var MatchReader_1 = require("./MatchReader");
+var reader = new MatchReader_1.MatchReader("football.csv");
 reader.read();
-var dateOfFirstMatch = reader.data[0][0];
-console.log("dsa", dateOfFirstMatch);
+// const dateOfFirstMatch = reader.data[0][0];
+// console.log("dsa", dateOfFirstMatch);
+console.log("dsa", reader.data);
 //MU win
-var manUnitedWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === "Man United" && match[5] === matchResults_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === "Man United" && match[5] === matchResults_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(manUnitedWins);
+// let manUnitedWins = 0;
+// for (let match of reader.data) {
+//   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+//     manUnitedWins++;
+//   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+//     manUnitedWins++;
+//   }
+// }
+// console.log(manUnitedWins);
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,14 +1,10 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./matchResults";
 
-//tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -17,19 +13,6 @@ export class CsvFileReader {
       })
       .split("\n")
       .map((row: string): string[] => row.split(","))
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            //type assertion,
-            row[5] as MatchResult, //'H','A','D'
-            row[6]
-          ];
-        }
-      );
+      .map(this.mapRow);
   }
 }
